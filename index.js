b6ecd4fa1ceb675f7aa31d3a66354712caeb7747
djimo-h <hocine.djimo@gmail.com>
2021-02-20 01:33:12 +0100

    1 file modified

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    //image: "./assets/vmSocks-green.jpg",
+    image: "./assets/vmSocks-green.jpg",
     inStock: true,
     inventory: 100,
 
