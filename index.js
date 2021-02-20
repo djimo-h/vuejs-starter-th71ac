6e2804e450e8464ce0ba21b60734d74550958b4a
djimo-h <hocine.djimo@gmail.com>
2021-02-20 01:05:17 +0100

    2 files modified

--- index.js
+++ index.js
@@ -5,7 +5,34 @@ var app = new Vue({
   data: {
     product: "Socks",
     image: "./assets/vmSocks-green.jpg",
-    //inStock: false
-    inventory: 0
+    inStock: true,
+    inventory: 100,
+
+    details: ["80% cotton", "20% polyster", "Gender-neutral"]
+  },
+
+  variants: [
+    {
+      variantId: 2234,
+      variantColor: "green",
+      varianImage: "./assets/vmSocks-green.jpg"
+    },
+    {
+      variantId: 2235,
+      variantColor: "blue",
+      varianImage: "./assets/vmSocks-blue.jpg"
+    }
+  ],
+
+  data: {
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
